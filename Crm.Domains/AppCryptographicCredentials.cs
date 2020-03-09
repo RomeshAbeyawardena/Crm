8d2bbf075282893a9a1a75683f9ddc849957b1e0
Anthony Abeyawardena <Anthony.Abeyawardena@connellsgroup.co.uk>
2020-03-09 12:23:17 +0000

    Moved ConfigCryptographicCredentials to own file

--- Crm.Domains/AppCryptographicCredentials.cs
+++ Crm.Domains/AppCryptographicCredentials.cs
@@ -18,15 +18,4 @@ namespace Crm.Domains
         public int TotalNumberOfBytes { get; set; }
         public Encoding Encoding { get; set; }
     }
-
-    public class ConfigCryptographicCredentials
-    {
-        public string Key { get; set; }
-        public string InitialVector { get; set; }
-        public string SymmetricAlgorithm { get; set; }
-        public KeyDerivationPrf KeyDerivationPrf { get; set; }
-        public int Iterations { get; set; }
-        public int TotalNumberOfBytes { get; set; }
-        public string Encoding { get; set; }
-    }
 }
---
+++ Crm.Domains/ConfigCryptographicCredentials.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+namespace Crm.Domains
+{
+    public class ConfigCryptographicCredentials
+    {
+        public string Key { get; set; }
+        public string InitialVector { get; set; }
+        public string SymmetricAlgorithm { get; set; }
+        public KeyDerivationPrf KeyDerivationPrf { get; set; }
+        public int Iterations { get; set; }
+        public int TotalNumberOfBytes { get; set; }
+        public string Encoding { get; set; }
+    }
+}
