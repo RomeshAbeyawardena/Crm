814ae7e4b753316275d693c6091cd3ea7276693e
Anthony Abeyawardena <Anthony.Abeyawardena@connellsgroup.co.uk>
2020-03-09 11:02:43 +0000

    Implemented data constants

--- Crm.Domains/ApplicationSettings.cs
+++ Crm.Domains/ApplicationSettings.cs
@@ -1,13 +1,21 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataConstants = Crm.Domains.Constants.Data;
 
 namespace Crm.Domains
 {
     public class ApplicationSettings
     {
+        public ApplicationSettings(IConfiguration configuration)
+        {
+            configuration.Bind(this);
+            configuration.GetConnectionString(DataConstants.ConnectionStringKey);
+        }
+
         public string DefaultConnectionString { get; set; }
         public IDictionary<string, AppCryptographicCredentials> EncryptionKeys { get; set; }
     }
