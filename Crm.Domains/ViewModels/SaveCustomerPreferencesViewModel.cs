a6b57a161e1477534ec6c0d03f1cc3618d5b4284
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-03-15 18:23:00 +0000

    sync view models

--- Crm.Domains/ViewModels/SaveCustomerPreferencesViewModel.cs
+++ Crm.Domains/ViewModels/SaveCustomerPreferencesViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Primitives;
+using System;
 using System.Collections.Generic;
 
 namespace Crm.Domains.ViewModels
@@ -6,5 +7,10 @@ namespace Crm.Domains.ViewModels
     public class SaveCustomerPreferencesViewModel
     {
         public Dictionary<string, bool> Preferences { get; set; } 
+        
+        public int CustomerId { get; set; }
+        public IDictionary<string, bool> CustomerPreferences { get; set; }
+        public DateTimeOffset FromDate { get; set; }
+        public DateTimeOffset NextCheckInDate { get; set; }
     }
 }
--- Crm.Domains/ViewModels/SavePreferenceViewModel.cs
+++ Crm.Domains/ViewModels/SavePreferenceViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,9 @@ namespace Crm.Domains.ViewModels
 {
     public class SavePreferenceViewModel
     {
-        
+        [Required]
+        public string Name { get; set; }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
     }
 }
